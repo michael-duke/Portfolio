f3a082727111327707aa19ab301bd2e7c6905b45
michael-duke <michael.21duke@gmail.com>
2022-08-16 21:56:49 +0300

    Make updates to my Portfolio
    - Add screenshots of projects replace mock images
    - Fix linter errors and update gitignore file to contain scss that I will use in future
    - Include the correct links for live and source links of each project

--- js/project-list.js
+++ js/project-list.js
@@ -1,13 +1,13 @@
 const projectList = [
   {
     id: 1,
-    title: 'tonic',
+    title: 'Africa Music Conference 2022',
     featuredImage: {
-      url: 'images/snapshoot-project-1.png',
-      alt: 'snapshoot-project-1',
+      url: 'images/snapshoot-project-1.jpg',
+      alt: 'capstone-project-1',
     },
     description: {
-      less: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+      less: 'The goal of the Microverse Capstone Project for Module I: (The Portal) is to create a website with an about page and a home page for an online conference event. Built with JavaScript, CSS, and HTML.',
       more: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.',
     },
     technologies: ['HTML', 'CSS', 'JavaScript'],
@@ -17,19 +17,19 @@ const projectList = [
       year: 2015,
     },
     link: {
-      source: '',
-      live: '',
+      source: 'https://github.com/michael-duke/Capstone-I',
+      live: 'https://michael-duke.github.io/Capstone-I/',
     },
   },
   {
     id: 2,
-    title: 'multi-post stories',
+    title: 'Awesome Books',
     featuredImage: {
       url: 'images/snapshoot-project-2.png',
       alt: 'snapshoot-project-2',
     },
     description: {
-      less: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+      less: 'This educational project for Microverse Module 2 involves creating a straightforward website that shows a list of books and lets you add and remove books from that list. The intention is to use modules and learn ES6 syntax to make it more orderly. built with JavaScript, CSS and HTML.',
       more: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.',
     },
     technologies: ['HTML', 'CSS', 'JavaScript'],
@@ -39,19 +39,19 @@ const projectList = [
       year: 2015,
     },
     link: {
-      source: '',
-      live: '',
+      source: 'https://github.com/michael-duke/Awesome-Books',
+      live: 'https://michael-duke.github.io/Awesome-Books/',
     },
   },
   {
     id: 3,
-    title: 'tonic',
+    title: 'Micro-Tasks',
     featuredImage: {
-      url: 'images/snapshoot-project-3.png',
+      url: 'images/snapshoot-project-3.jpg',
       alt: 'snapshoot-project-3',
     },
     description: {
-      less: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+      less: 'Micro-Taks is an interactive project where you can create different daily task list. Functionalities include adding and removing a task, as well as a sortable drag and drop',
       more: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.',
     },
     technologies: ['HTML', 'CSS', 'JavaScript'],
@@ -61,8 +61,8 @@ const projectList = [
       year: 2015,
     },
     link: {
-      source: '',
-      live: '',
+      source: 'https://github.com/michael-duke/Micro-Tasks',
+      live: 'https://michael-duke.github.io/Micro-Tasks/',
     },
   },
   {
