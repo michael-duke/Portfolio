2b003649e84c025ec5a30c5eaf4635339e557db6
michael-duke <michael.21duke@gmail.com>
2023-01-21 00:56:02 +0300

    Correct client key for all projects and add Final Capstone

--- js/project-list.js
+++ js/project-list.js
@@ -8,11 +8,11 @@ const projectList = [
     },
     description: {
       less: 'The goal of the Microverse Capstone Project for Module I: (The Portal) is to create a website with an about page and a home page for an online conference event. Built with JavaScript, CSS, and HTML.',
-      more: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.',
+      more: 'The goal of the Microverse Capstone Project for Module I: (The Portal) is to create a website with an about page and a home page for an online conference event. Built with JavaScript, CSS, and HTML.',
     },
-    technologies: ['HTML', 'CSS', 'JavaScript'],
+    technologies: ['HTML', 'CSS', 'JavaScript (ES6)'],
     projectDetail: {
-      client: 'canopy',
+      client: 'microverse',
       role: 'front end dev',
       year: 2022,
     },
@@ -30,11 +30,11 @@ const projectList = [
     },
     description: {
       less: 'This educational project for Microverse Module 2 involves creating a straightforward website that shows a list of books and lets you add and remove books from that list. The intention is to use modules and learn ES6 syntax to make it more orderly. built with JavaScript, CSS and HTML.',
-      more: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.',
+      more: 'This educational project for Microverse Module 2 involves creating a straightforward website that shows a list of books and lets you add and remove books from that list. The intention is to use modules and learn ES6 syntax to make it more orderly. built with JavaScript, CSS and HTML.',
     },
-    technologies: ['HTML', 'Tailwind CSS', 'JavaScript'],
+    technologies: ['HTML', 'Tailwind CSS', 'JavaScript (ES6)'],
     projectDetail: {
-      client: 'canopy',
+      client: 'microverse',
       role: 'front end dev',
       year: 2022,
     },
@@ -52,11 +52,11 @@ const projectList = [
     },
     description: {
       less: 'Micro-Taks is an interactive project where you can create different daily task list. Functionalities include adding and removing a task, as well as a sortable drag and drop',
-      more: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.',
+      more: 'Micro-Taks is an interactive project where you can create different daily task list. Functionalities include adding and removing a task, as well as a sortable drag and drop',
     },
-    technologies: ['HTML', 'Tailwind CSS', 'JavaScript', 'Webpack'],
+    technologies: ['HTML', 'Tailwind CSS', 'JavaScript (ES6)', 'Webpack'],
     projectDetail: {
-      client: 'canopy',
+      client: 'microverse',
       role: 'back end dev',
       year: 2022,
     },
@@ -74,12 +74,12 @@ const projectList = [
     },
     description: {
       less: 'This JavaScript capstone project is built on the FreeToGame API, an external API, and Microverse\'s in-house Involvement API. The cards display the game information that was retrieved from the API. The user has the option to like and comment on the game cards.',
-      more: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.',
+      more: 'This JavaScript capstone project is built on the FreeToGame API, an external API, and Microverse\'s in-house Involvement API. The cards display the game information that was retrieved from the API. The user has the option to like and comment on the game cards.',
     },
     technologies: ['HTML', 'Taiwind CSS', 'ES6', 'Webpack'],
     projectDetail: {
-      client: 'canopy',
-      role: 'fullstack dev',
+      client: 'microverse',
+      role: 'front end dev',
       year: 2022,
     },
     link: {
@@ -96,11 +96,11 @@ const projectList = [
     },
     description: {
       less: 'Heroic is a web app that will display a list of random 20 heroes from SuperHero Search API. User can click on a hero card and have all his details in a separate page, and filter by publisher as well.',
-      more: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.',
+      more: 'Heroic is a web app that will display a list of random 20 heroes from SuperHero Search API. User can click on a hero card and have all his details in a separate page, and filter by publisher as well.',
     },
     technologies: ['HTML', 'Tailwind CSS', 'React', 'Redux', 'React Router','Redux-thunk'],
     projectDetail: {
-      client: 'canopy',
+      client: 'microverse',
       role: 'fullstack dev',
       year: 2022,
     },
@@ -118,17 +118,39 @@ const projectList = [
     },
     description: {
       less: 'Carpe Diem is a mobile web application where you can manage your budget: you have a list of transactions associated with a category, so that you can see how much money you spent and on what. The app is built with Ruby on Rails and PostgreSQL.',
-      more: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.',
+      more: 'LCarpe Diem is a mobile web application where you can manage your budget: you have a list of transactions associated with a category, so that you can see how much money you spent and on what. The app is built with Ruby on Rails and PostgreSQL.',
     },
     technologies: ['Tailwind CSS', 'Ruby', 'Ruby on Rails', 'PostgreSQL'],
     projectDetail: {
-      client: 'canopy',
+      client: 'microverse',
       role: 'fullstack dev',
       year: 2022,
     },
     link: {
       source: 'https://github.com/michael-duke/Capstone-V',
-      live: 'https://carpe-diem.onrender.com/',
+      live: 'https://carpe-diem.up.railway.app/',
+    },
+  },
+  {
+    id: 7,
+    title: 'El Dorado',
+    featuredImage: {
+      url: 'images/snapshoot-project-7.png',
+      alt: 'snapshoot-project-7',
+    },
+    description: {
+      less: 'El Dorado Frontend is a luxury car rental application where the user can register new accounts, login and reserve a set of different cars. Admin user can also create new cars and mark them as available/removed on the list. Built using a connection to two different repos, including Back-end(Rails) and Front-end(React/Redux).',
+      more: 'El Dorado Frontend is a luxury car rental application where the user can register new accounts, login and reserve a set of different cars. Admin user can also create new cars and mark them as available/removed on the list. Built using a connection to two different repos, including Back-end(Rails) and Front-end(React/Redux).',
+    },
+    technologies: ['Tailwind CSS', 'Ruby', 'Ruby on Rails', 'PostgreSQL', 'React', 'Redux', 'React Router','Redux-toolkit', 'REST API'],
+    projectDetail: {
+      client: 'microverse',
+      role: 'fullstack dev',
+      year: 2023,
+    },
+    link: {
+      source: 'https://github.com/michael-duke/ElDorado-Frontend',
+      live: 'https://eldorado-rental.netlify.app/',
     },
   }
 ];
