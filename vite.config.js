35f363e5150420a1c178a2f2000c59b18ecd73b6
Michael Mesfin <michael.21duke@gmail.com>
2026-04-30 02:29:30 -0400

    Implement path definition in vite config install necessary deps and ui libraries

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
